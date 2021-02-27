12260e821242caf516f1bb3feb789a03a9b908b1
Matt Galvin <matthewgalvin908@gmail.com>
2021-02-27 21:21:54 +0100

    Use Registry to keep track of multipleOnlinePageProducerConsumer processes

--- lib/scraper/application.ex
+++ lib/scraper/application.ex
@@ -18,14 +18,25 @@ defmodule Scraper.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {Registry, keys: :unique, name: ProducerConsumerRegistry},
       PageProducer,
       # ProducerConsumer must be started before consumers so
       # OnlinePageProducerConsumer is listed befor PageConsumerSupervisor
-      OnlinePageProducerConsumer,
+      producer_consumer_spec(id: 1),
+      producer_consumer_spec(id: 2),
       PageConsumerSupervisor
     ]
 
     opts = [strategy: :one_for_one, name: Scraper.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  b/c we're going to add more processes of the same type, line 21 uses Registry
+  to keep track of them and we add this function for easy id and name assignment
+  """
+  def producer_consumer_spec(id: id) do
+    id = "online_page_producer_consumer_#{id}"
+    Supervisor.child_spec({OnlinePageProducerConsumer, id}, id: id)
   end
 end
