f843ba61ee9a7d0c6b8b83efa26a78c5122343a3
Matt Galvin <matthewgalvin908@gmail.com>
2021-02-27 14:54:37 +0100

    Add consumer

--- lib/page_producer.ex
+++ lib/page_producer.ex
@@ -9,7 +9,7 @@ defmodule PageProducer do
 
   @doc """
   valid first elements for the retun value  are producer, consumer, or
-  producer_consumer
+  producer_consumer- determines type of process.
   """
   def init(initial_state) do
     Logger.info("PageProducer init")
@@ -20,7 +20,7 @@ defmodule PageProducer do
   handle_demand/2 callback is required for producer and producer_consumer
   processes
   This func is invoked when a consumer asks for events, demand = number of
-  events requestd by consumers and state = internal state of the producer
+  events requestd by consumers and state = internal state of the producer.
   """
   def handle_demand(demand, state) do
     Logger.info("Received demand for #{demand} pages")
